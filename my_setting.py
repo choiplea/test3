1becd43f81b65a4e748898721ecf8076921951e1
choi bada <choiplea@gmail.com>
2022-05-11 13:16:42 +0900

    Add: Django Project Setting

---
+++ my_setting.py
@@ -0,0 +1,12 @@
+DATABASES = {
+'default' : {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'choibada1',
+'USER': 'pleasy',
+'PASSWORD': '1978',
+'HOST': '127.0.0.1',
+'PORT': '3306',
+}
+}
+
+SECRET_KEY = 'django-insecure-x8+y@9(tn8g+5r@f=bd*02=+u&fzbly89o3lcxa968%+47l*nm'
